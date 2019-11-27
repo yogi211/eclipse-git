540641be8169041b0f7f1755b2d9b2885d2f234e
yogi <yuyiheshu@localhost>
2019-11-27 23:27:09 +0900

    First commit.

---
+++ src/test00/Keisan.java
@@ -0,0 +1,11 @@
+package test00;
+
+public class Keisan {
+
+	public static void formula() {
+	  int sum = 0;
+			for (int i = 1; i <= 10; i++)
+			  sum += i;
+			System.out.println(sum);
+	}
+}
---
+++ src/test00/testfor.java
@@ -0,0 +1,8 @@
+package test00;
+
+public class testfor {
+
+	public static void main(String[] args) {
+	Keisan.formula();
+	}
+}
